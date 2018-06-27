83726434e8eeb84a4a258d333441a4c165ddda85
David Hopkins <dlhopkins2@gmail.com>
2018-06-27 21:55:35 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,10 +19,17 @@ def select_value_and_count_of_most_prolific_species
   GROUP BY species
   ORDER BY most_prolific_species DESC
   LIMIT 1;"
+
+  #More condensed:
+  #SELECT species, COUNT(*) 
+  #FROM characters 
+  #GROUP BY species 
+  #ORDER BY COUNT(species) DESC 
+  #LIMIT 1;
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT name, subgenres.name
+  "SELECT authors.name, subgenres.name
   FROM authors
   JOIN series
     ON series.author_id = authors.id
@@ -33,10 +40,15 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
+  JOIN books 
+  ON books.series_id = series.id 
+  JOIN character_books 
+  ON character_books.book_id = books.id
   JOIN characters
-  ON series.id = characters.series_id
+  ON character_books.character_id = characters.id
+  WHERE characters.species = 'human' 
   GROUP BY series.title
-  ORDER BY COUNT(characters.species)
+  ORDER BY COUNT(characters.species) DESC
   LIMIT 1;"
 end
 
